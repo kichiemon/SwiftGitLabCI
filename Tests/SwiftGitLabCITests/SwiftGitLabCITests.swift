0a3a906cad3c40978e7d0f77ccce356b43483d75
iKichiemon <1990.iku.t@gmail.com>
2019-02-14 13:25:37 +0900

    test: failure

--- Tests/SwiftGitLabCITests/SwiftGitLabCITests.swift
+++ Tests/SwiftGitLabCITests/SwiftGitLabCITests.swift
@@ -2,11 +2,19 @@ import XCTest
 @testable import SwiftGitLabCI
 
 final class SwiftGitLabCITests: XCTestCase {
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(SwiftGitLabCI().text, "Hello, World!")
+    }
+
+    func testFailureExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftGitLabCI().text, "not equal!")
     }
 
     static var allTests = [
